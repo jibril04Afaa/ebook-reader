e3c2ebcb496c8c4a78df708b45859abb537bab64
jibril04Afaa <128831088+jibril04Afaa@users.noreply.github.com>
2024-12-24 04:30:00 -0500

    added search ui

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,19 +1,47 @@
-
+import { CiSearch } from "react-icons/ci";
 
 const SearchBar = () => {
   return (
-    <div>
+    <div className="border-2 p-4">
       
       {/* Inner Container */}
-      <div className="w-full">
-      {/* Title/Label */}
+      <div className="">
+
+        {/* Title/Label */}
         <div>
           <label htmlFor="search-bar"></label>
         </div>
 
-      {/* Search Bar */}
-        <div>
-          <input type="text" className="border-2"/>
+        {/* Search Bar */}
+        <div className="flex items-center border-2 rounded-xl">
+          <CiSearch className="text-3xl"/>
+          <input type="text" className="h-full"/>
+          <button className="rounded-lg pl-4 pr-4 pt-2 pb-2 bg-gold font-bold">Search</button>
+        </div>
+
+      </div>
+
+      {/* Search Results // display only when search bar is not empty */}
+      <div className="">
+        {/* Inner Container */}
+        <div className="">
+          {/* Search Results */}
+          <div className="border-2 rounded-lg mt-4 p-2 bg-gold">
+            {/* First Search Result */}
+            <div className="border-b-2 pt-2 pb-2">
+              <p className="font-bold">The Great Gatsby</p>
+            </div>
+
+            {/* Second Search Result */}
+            <div className="border-b-2 pt-2 pb-2">
+              <p className="font-bold"> The Great Galveston Hurricane</p>
+            </div>
+
+            {/* Third Search Result */}
+            <div className="border-b-2 pt-2 pb-2">
+              <p className="font-bold">The Great Greek Mediterranean G..</p>
+            </div>
+          </div>
         </div>
       </div>
 
